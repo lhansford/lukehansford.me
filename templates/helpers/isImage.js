3be7999d46613f33b013e4a86f21c33f7bf66e7b
Luke Hansford <mail@lukehansford.me>
2018-08-13 23:12:35 +0800

    Add polyfill for endswith

--- templates/helpers/isImage.js
+++ templates/helpers/isImage.js
@@ -1,5 +1,13 @@
 const Handlebars = require('handlebars');
 
 module.exports = function(fileName) {
+  if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(search, this_len) {
+      if (this_len === undefined || this_len > this.length) {
+        this_len = this.length;
+      }
+      return this.substring(this_len - search.length, this_len) === search;
+    };
+  }
   return fileName.endsWith('.jpg');
 }
