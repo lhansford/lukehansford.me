63858b01232c9c0e3c2d3d1ac4c3809e697c1383
Luke Hansford <mail@lukehansford.me>
2020-09-27 18:56:58 +0200

    Adjust analytics to work with Next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,23 @@
 import App from 'next/app';
+import Router from 'next/router';
 
 import '../css/main.css';
 import '../css/font-awesome-4.2.0/css/font-awesome.min.css';
 
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
+declare global {
+  interface Window {
+    goatcounter?: {
+      count: (props: { path: string }) => void;
+    };
+  }
+}
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
+Router.events.on('routeChangeComplete', (url) => {
+  if (window && window.goatcounter) {
+    window.goatcounter.count({
+      path: url,
+    });
+  }
+});
 
 export default App;
